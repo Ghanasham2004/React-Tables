cb968ed3e6f62d98ac98ed6dcff33561b5095243
Ghansham Irashetti <105876844+Ghanasham2004@users.noreply.github.com>
2023-08-16 20:59:45 +0530

    Use Of Grouped Columns

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -4,14 +4,23 @@ import { COLUMNS } from "./columns";
 import MOCK_DATA from "./MOCK_DATA.json";
 import "./table.css";
 const BasicTable = () => {
-  const columns = useMemo(() => COLUMNS, []);
+  const columns = useMemo(() => COLUMNS, []); // this is the first way to memoize the columns and data and it is the recommended way to do it
+
+  // const columns = useMemo(() => GROUPED_COLUMNS, []); // This is the second way to memoize the columns and data and it is the recommended way to do it .
   const data = useMemo(() => MOCK_DATA, []);
   const tableInstance = useTable({
     columns,
     data,
   });
-  const { getTableProps, getTableBodyProps, headerGroups,footerGroups, rows, prepareRow } =
-    tableInstance;
+  
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+  } = tableInstance; // this is the table instance that we get from the useTable hook and we can destructure the properties from it that we need to build our table with react-table library and we can pass these properties to the elements that we want to render in our table like the table body, table rows, table cells, table headers and table footers. 
   return (
     <table {...getTableProps()}>
       <thead>
@@ -27,7 +36,7 @@ const BasicTable = () => {
         {" "}
         {/* // this is the body of the table where we will loop through the rows and cells and render the data in the table */}
         {rows.map((row) => {
-          prepareRow(row); // this is a function that prepares the row for rendering and must be called for each row before getting the row props and cell props otherwise it will throw an error 
+          prepareRow(row); // this is a function that prepares the row for rendering and must be called for each row before getting the row props and cell props otherwise it will throw an error
           return (
             <tr {...row.getRowProps()}>
               {row.cells.map((cell) => {
@@ -39,17 +48,13 @@ const BasicTable = () => {
       </tbody>
       {/* // this is the footer of the table where we will loop through the footer groups and render the data in the table  */}
       <tfoot>
-          {footerGroups.map(footerGroup => (
-              <tr {...footerGroup.getFooterGroupProps()}>
-                  {footerGroup.headers.map(column => (
-                      <td {...column.getFooterProps()}>
-                          {
-                              column.render('Footer')
-                          }
-                      </td>
-                  ))}
-              </tr>
-          ))}
+        {footerGroups.map((footerGroup) => (
+          <tr {...footerGroup.getFooterGroupProps()}>
+            {footerGroup.headers.map((column) => (
+              <td {...column.getFooterProps()}>{column.render("Footer")}</td>
+            ))}
+          </tr>
+        ))}
       </tfoot>
     </table>
   );
